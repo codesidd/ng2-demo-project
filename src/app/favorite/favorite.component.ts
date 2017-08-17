f67546997f7078bf72f7e35a00b6e2696995d583
Siddharth Aggarwal <codesidd@users.noreply.github.com>
2017-08-17 13:17:21 +0530

    feat(repository): [20170817] viewEncapsulation

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -14,7 +14,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   }
     `
   ],
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FavoriteComponent {
   @Input('isFavorite') isSelected: boolean;
