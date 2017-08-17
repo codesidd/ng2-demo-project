f40b190761ca4ba15731070327464669ca5ac8a8
Siddharth Aggarwal <codesidd@users.noreply.github.com>
2017-08-17 09:28:22 +0530

    feat(repository): Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  imageUrl = "http://lorempixel.com/400/200";
+  
+  post = {
+    title: "Title",
+    isFavorite: true
+  }
+
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
+    console.log("Favorite changed: ", eventArgs);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { SummaryPipe } from './summary.pipe';
+import { AuthorsService } from './authors.service';
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    TitleCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
